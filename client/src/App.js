1af6346edbb425bb8d5c20ef945cce87b4a17794
DEO <DEO@DESKTOP-1AAKV4M>
2021-02-17 15:16:59 +0200

    Routes Added to App

--- client/src/App.js
+++ client/src/App.js
@@ -1,10 +1,24 @@
 import React, { Fragment } from 'react';
+import { BrowserRouter as Router, Route, Switch } from 'react-router-dom';
+import Navbar from './components/layout/Navbar';
+import Landing from './components/layout/Landing';
+import Register from './components/Auth/Register';
+import Login from './components/Auth/Login';
 import './App.css';
 
 const App = () => (
-  <Fragment>
-    <h1>App Component</h1>
-  </Fragment>
+  <Router>
+    <Fragment>
+      <Navbar />
+      <Route exact path='/' component={Landing} />
+      <section>
+        <Switch>
+          <Route exact path='/login' component={Login} />
+          <Route exact path='/register' component={Register} />
+        </Switch>
+      </section>
+    </Fragment>
+  </Router>
 );
 
 export default App;
