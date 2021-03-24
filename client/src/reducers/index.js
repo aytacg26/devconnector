a3340d420a477fa2cf9a74bcd5b05106ffd46021
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-24 13:37:38 +0200

    authReducer and alertReducer added to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,6 +1,8 @@
 import { combineReducers } from 'redux';
-import alert from './alert';
+import alertReducer from './alert';
+import authReducer from './auth';
 
 export default combineReducers({
-  alert,
+  alertReducer,
+  authReducer,
 });
