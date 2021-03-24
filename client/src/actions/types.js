2d4c506cbf4ab3cdd6bd9fee97d29696a351de1a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-24 13:35:54 +0200

    redux registration types added

--- client/src/actions/types.js
+++ client/src/actions/types.js
@@ -1,2 +1,6 @@
 export const SET_ALERT = 'SET_ALERT';
 export const REMOVE_ALERT = 'REMOVE_ALERT';
+export const REGISTER_SUCCESS = 'REGISTER_SUCCESS';
+export const REGISTER_FAIL = 'REGISTER_FAIL';
+export const USER_LOADED = 'USER_LOADED'; //This will run on success
+export const AUTH_ERROR = 'AUTH_ERROR'; //If there is any auth problem, if token does not match, this action type will take place
