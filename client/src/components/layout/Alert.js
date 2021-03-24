228352a821beb402a4dd71bf732787f4d99dc818
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-24 13:36:54 +0200

    alert component created

--- client/src/components/layout/Alert.js
+++ client/src/components/layout/Alert.js
@@ -2,9 +2,10 @@ import React from 'react';
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 
-const Alert = (props) => {
-  const alerts = useSelector((state) => state.alert);
-  let alert = '';
+const Alert = () => {
+  //We get the initialState of alertReducer by using useSelector. To reach actions of a reducer to dipatch them, we need to use useDispatch from react-redux
+  const alerts = useSelector((state) => state.alertReducer);
+  let alert = null;
   if (alerts !== null && alerts.length > 0) {
     alert = alerts.map((al) => (
       <div key={al.id} className={`alert alert-${al.alertType}`}>
