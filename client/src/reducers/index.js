ea2ce0b92e35a6b37809c4f7c712e9bbb8259d16
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-28 19:19:19 +0300

    profileReducer added to rootReducer

--- client/src/reducers/index.js
+++ client/src/reducers/index.js
@@ -1,8 +1,10 @@
 import { combineReducers } from 'redux';
 import alertReducer from './alert';
 import authReducer from './auth';
+import profileReducer from './profile';
 
 export default combineReducers({
   alertReducer,
   authReducer,
+  profileReducer,
 });
