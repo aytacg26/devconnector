fa5a0b0cc49ad876c0332496de975645284dabb6
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-28 19:16:38 +0300

    getProfile action method created

---
+++ client/src/actions/profile.js
@@ -0,0 +1,23 @@
+import { GET_PROFILE, PROFILE_ERROR } from './types';
+import { setAlert } from './alert';
+import axios from 'axios';
+
+//GET THE CURRENT USER PROFILE
+export const getCurrentProfile = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/profile/me');
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
