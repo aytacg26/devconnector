dd0a2c9ae75895b635927dd3a7a2042c89c04dc0
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-03-29 20:52:45 +0300

    create profile action completed

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -21,3 +21,50 @@ export const getCurrentProfile = () => async (dispatch) => {
     });
   }
 };
+
+//Create or Update Profile
+export const createProfile = (formData, history, edit = false) => async (
+  dispatch
+) => {
+  try {
+    const config = {
+      headers: {
+        'Content-Type': 'application/json',
+      },
+    };
+
+    const res = await axios.post('/api/profile', formData, config);
+
+    dispatch({
+      type: GET_PROFILE,
+      payload: res.data,
+    });
+
+    const message = edit
+      ? 'Changes saved and Profile Updated'
+      : 'Profile Created Successfully';
+
+    dispatch(setAlert(message, 'success'));
+
+    if (!edit) {
+      history.push('/dashboard');
+    }
+  } catch (error) {
+    //For validation errors :
+    const errors = error.response.data.errors;
+    console.log(error.response.data.errors);
+
+    if (errors) {
+      errors.forEach((error) => dispatch(setAlert(error.msg, 'danger', 6000)));
+    }
+
+    //For creation or update errors
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
