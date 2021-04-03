3c4bf9074ca8423166cec15e727fefb89de61465
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-03 12:36:19 +0300

    Edit Profile Component Created

---
+++ client/src/components/profile-form/EditProfile.js
@@ -0,0 +1,257 @@
+import React, { Fragment, useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
+import { Link, withRouter } from 'react-router-dom';
+import { createProfile, getCurrentProfile } from '../../actions/profile';
+
+const EditProfile = ({ history }) => {
+  const [formData, setFormData] = useState({
+    company: '',
+    website: '',
+    location: '',
+    status: '',
+    skills: '',
+    githubusername: '',
+    bio: '',
+    twitter: '',
+    facebook: '',
+    linkedin: '',
+    youtube: '',
+    instagram: '',
+  });
+
+  const [displaySocialInputs, setDisplaySocialInput] = useState(false);
+  const editProfileState = useSelector((state) => ({
+    profile: state.profileReducer.profile,
+    loading: state.profileReducer.loading,
+  }));
+
+  const { profile, loading } = editProfileState;
+
+  useEffect(() => {
+    dispatch(getCurrentProfile());
+
+    if (profile !== null) {
+      setFormData({
+        company: loading || !profile.company ? '' : profile.company,
+        website: loading || !profile.website ? '' : profile.website,
+        location: loading || !profile.location ? '' : profile.location,
+        status: loading || !profile.status ? '' : profile.status,
+        skills: loading || !profile.skills ? '' : profile.skills,
+        githubusername:
+          loading || !profile.githubusername ? '' : profile.githubusername,
+        bio: loading || !profile.bio ? '' : profile.bio,
+        twitter: loading || !profile.social ? '' : profile.social.twitter,
+        facebook: loading || !profile.social ? '' : profile.social.facebook,
+        linkedin: loading || !profile.social ? '' : profile.social.linkedin,
+        youtube: loading || !profile.social ? '' : profile.social.youtube,
+        instagram: loading || !profile.social ? '' : profile.social.instagram,
+      });
+    }
+  }, [loading]);
+
+  const {
+    company,
+    website,
+    location,
+    status,
+    skills,
+    githubusername,
+    bio,
+    twitter,
+    facebook,
+    linkedin,
+    youtube,
+    instagram,
+  } = formData;
+
+  const dispatch = useDispatch();
+
+  const handleChange = (e) => {
+    const { name, value } = e.target;
+
+    setFormData({
+      ...formData,
+      [name]: value,
+    });
+  };
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    dispatch(createProfile(formData, history, true));
+  };
+
+  return (
+    <Fragment>
+      <h1 className='large text-primary'>Edit Your Profile</h1>
+      <p className='lead'>
+        <i className='fas fa-user'></i> Let's get some information to make your
+        profile stand out
+      </p>
+      <small>* = required field</small>
+      <form className='form' onSubmit={handleSubmit}>
+        <div className='form-group'>
+          <select name='status' value={status} onChange={handleChange}>
+            <option value='0'>* Select Professional Status</option>
+            <option value='Developer'>Developer</option>
+            <option value='Junior Developer'>Junior Developer</option>
+            <option value='Senior Developer'>Senior Developer</option>
+            <option value='Manager'>Manager</option>
+            <option value='Student or Learning'>Student or Learning</option>
+            <option value='Instructor'>Instructor or Teacher</option>
+            <option value='Intern'>Intern</option>
+            <option value='Other'>Other</option>
+          </select>
+          <small className='form-text'>
+            Give us an idea of where you are at in your career
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Company'
+            name='company'
+            value={company}
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            Could be your own company or one you work for
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Website'
+            name='website'
+            value={website}
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            Could be your own or a company website
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Location'
+            name='location'
+            value={location}
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            City & state suggested (eg. Boston, MA)
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='* Skills'
+            name='skills'
+            value={skills}
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            Please use comma separated values (eg. HTML,CSS,JavaScript,PHP)
+          </small>
+        </div>
+        <div className='form-group'>
+          <input
+            type='text'
+            placeholder='Github Username'
+            name='githubusername'
+            value={githubusername}
+            onChange={handleChange}
+          />
+          <small className='form-text'>
+            If you want your latest repos and a Github link, include your
+            username
+          </small>
+        </div>
+        <div className='form-group'>
+          <textarea
+            placeholder='A short bio of yourself'
+            name='bio'
+            value={bio}
+            onChange={handleChange}
+          ></textarea>
+          <small className='form-text'>Tell us a little about yourself</small>
+        </div>
+
+        <div className='my-2'>
+          <button
+            type='button'
+            className='btn btn-light'
+            onClick={() => setDisplaySocialInput(!displaySocialInputs)}
+          >
+            Add Social Network Links
+          </button>
+          <span>Optional</span>
+        </div>
+
+        {displaySocialInputs ? (
+          <Fragment>
+            <div className='form-group social-input'>
+              <i className='fab fa-twitter fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Twitter URL'
+                name='twitter'
+                value={twitter}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-facebook fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Facebook URL'
+                name='facebook'
+                value={facebook}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-youtube fa-2x'></i>
+              <input
+                type='text'
+                placeholder='YouTube URL'
+                name='youtube'
+                value={youtube}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-linkedin fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Linkedin URL'
+                name='linkedin'
+                value={linkedin}
+                onChange={handleChange}
+              />
+            </div>
+
+            <div className='form-group social-input'>
+              <i className='fab fa-instagram fa-2x'></i>
+              <input
+                type='text'
+                placeholder='Instagram URL'
+                name='instagram'
+                value={instagram}
+                onChange={handleChange}
+              />
+            </div>
+          </Fragment>
+        ) : null}
+        <input type='submit' className='btn btn-primary my-1' />
+        <Link to='/dashboard' className='btn btn-light my-1'>
+          Go Back
+        </Link>
+      </form>
+    </Fragment>
+  );
+};
+
+export default withRouter(EditProfile);
