f5d526e7b7bf022876aa9cb4f060b0780bbf59c5
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-03 12:37:01 +0300

    updated becuase of a bug

--- client/src/components/routing/PrivateRoute.js
+++ client/src/components/routing/PrivateRoute.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Route, Redirect } from 'react-router-dom';
 import { useSelector } from 'react-redux';
+import Spinner from '../layout/Spinner';
 
 const PrivateRoute = ({ component: Component, ...rest }) => {
   const authState = useSelector((state) => ({
@@ -8,12 +9,17 @@ const PrivateRoute = ({ component: Component, ...rest }) => {
     loading: state.authReducer.loading,
   }));
   const { isAuthenticated, loading } = authState;
+  const token = localStorage.token;
+
+  if (loading) {
+    return <Spinner />;
+  }
 
   return (
     <Route
       {...rest}
       render={(props) =>
-        !isAuthenticated && !loading ? (
+        !isAuthenticated && !token ? (
           <Redirect to='/login' />
         ) : (
           <Component {...props} />
