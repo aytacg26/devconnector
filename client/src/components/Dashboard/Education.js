b67de55dde838c31ba68d289d5eab2d462ba8d05
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-03 16:27:02 +0300

    Education List Component Created

---
+++ client/src/components/Dashboard/Education.js
@@ -0,0 +1,51 @@
+import React, { Fragment } from 'react';
+import { useDispatch } from 'react-redux';
+import Moment from 'react-moment';
+import PropTypes from 'prop-types';
+
+const Education = ({ education }) => {
+  const educations = education.map((edu) => (
+    <tr key={edu._id}>
+      <td>{edu.school}</td>
+      <td className='hide-sm'>{edu.degree}</td>
+      <td>
+        <Moment format='YYYY/MM/DD'>{edu.from}</Moment> -{' '}
+        {edu.to === null ? (
+          'Current'
+        ) : (
+          <Moment format='YYYY/MM/DD'>{edu.to}</Moment>
+        )}
+      </td>
+      <td>
+        <button className='btn btn-danger'>Delete</button>
+      </td>
+    </tr>
+  ));
+
+  return (
+    <Fragment>
+      <h2 className='my-2'>Education Credentials</h2>
+      <table className='table'>
+        <thead>
+          <tr>
+            <th style={{ textAlign: 'center' }}>School</th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Degree
+            </th>
+            <th className='hide-sm' style={{ textAlign: 'center' }}>
+              Years
+            </th>
+            <th />
+          </tr>
+        </thead>
+        <tbody>{educations}</tbody>
+      </table>
+    </Fragment>
+  );
+};
+
+Education.propTypes = {
+  education: PropTypes.array.isRequired,
+};
+
+export default Education;
