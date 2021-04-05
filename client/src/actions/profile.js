39863f7d87a679fe4231a1038e0de546169a2ce7
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-05 20:26:10 +0300

    deleteExperience, deleteEducation actions added.

--- client/src/actions/profile.js
+++ client/src/actions/profile.js
@@ -1,4 +1,10 @@
-import { GET_PROFILE, PROFILE_ERROR, UPDATE_PROFILE } from './types';
+import {
+  CLEAR_PROFILE,
+  DELETE_ACCOUNT,
+  GET_PROFILE,
+  PROFILE_ERROR,
+  UPDATE_PROFILE,
+} from './types';
 import { setAlert } from './alert';
 import axios from 'axios';
 
@@ -140,5 +146,76 @@ export const addEducation = (formData, history) => async (dispatch) => {
         status: error.response.status,
       },
     });
+  }
+};
+
+//Delete Experience
+export const deleteExperience = (id) => async (dispatch) => {
+  try {
+    //In case of delete processes, make sure to ask user if s/he is sure about the delete process and after approval of user call this action
+    const res = await axios.delete(`/api/profile/experience/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data, //we will get latest profile and present it.
+    });
+    dispatch(setAlert('Experience Deleted', 'danger'));
+  } catch (error) {
+    //For creation or update errors
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+//Delete Education
+export const deleteEducation = (id) => async (dispatch) => {
+  try {
+    //In case of delete processes, make sure to ask user if s/he is sure about the delete process and after approval of user call this action
+    const res = await axios.delete(`/api/profile/education/${id}`);
+
+    dispatch({
+      type: UPDATE_PROFILE,
+      payload: res.data, //we will get latest profile and present it.
+    });
+    dispatch(setAlert('Education Deleted', 'danger'));
+  } catch (error) {
+    //For creation or update errors
+    dispatch({
+      type: PROFILE_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
+
+//Delete Account && Profile (we do not need id here because backend will know the account from token)
+export const deleteAccount = () => async (dispatch) => {
+  if (window.confirm('Are you sure? This can NOT be undone!')) {
+    try {
+      const res = await axios.delete(`/api/profile`);
+
+      dispatch({
+        type: CLEAR_PROFILE,
+      });
+      dispatch({ type: DELETE_ACCOUNT });
+      dispatch(
+        setAlert('Your account has been permanently deleted.', 'danger')
+      );
+    } catch (error) {
+      dispatch({
+        type: PROFILE_ERROR,
+        payload: {
+          msg: error.response.statusText,
+          status: error.response.status,
+        },
+      });
+    }
   }
 };
