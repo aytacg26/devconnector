fa59a56db51e12d107b2265aa495024c343b5c8b
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-05 20:27:54 +0300

    DELETE_ACCOUNT case added

--- client/src/reducers/auth.js
+++ client/src/reducers/auth.js
@@ -6,6 +6,7 @@ import {
   LOGIN_SUCCESS,
   LOGIN_FAIL,
   LOGOUT,
+  DELETE_ACCOUNT,
 } from '../actions/types';
 
 const initialState = {
@@ -33,6 +34,7 @@ const authReducer = (state = initialState, action) => {
     case AUTH_ERROR:
     case LOGIN_FAIL:
     case LOGOUT:
+    case DELETE_ACCOUNT:
       localStorage.removeItem('token');
       return {
         ...state,
