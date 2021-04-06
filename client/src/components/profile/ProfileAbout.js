65a26be751a9fda3c35d7cc46ee68ea31dbe3c2a
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-06 16:17:26 +0300

    Profile About Component Created

---
+++ client/src/components/profile/ProfileAbout.js
@@ -0,0 +1,35 @@
+import React, { Fragment } from 'react';
+import PropTypes from 'prop-types';
+
+const ProfileAbout = ({ profile }) => {
+  const { skills, bio, user } = profile;
+  const { name } = user;
+  const firstname = name.trim().split(' ')[0];
+
+  return (
+    <div className='profile-about bg-light p-2'>
+      {bio && (
+        <Fragment>
+          <h2 className='text-primary'>{`${firstname}'s Bio`}</h2>
+          <p>{bio}</p>
+          <div className='line'></div>
+        </Fragment>
+      )}
+
+      <h2 className='text-primary'>Skill Set</h2>
+      <div className='skills'>
+        {skills.map((skill, index) => (
+          <div className='p-1' key={`skill-${firstname}-${index}`}>
+            <i className='fa fa-check'></i> {skill}
+          </div>
+        ))}
+      </div>
+    </div>
+  );
+};
+
+ProfileAbout.propTypes = {
+  profile: PropTypes.object.isRequired,
+};
+
+export default ProfileAbout;
