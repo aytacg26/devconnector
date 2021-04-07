3b593602420b8811dbadf8cf55dfe04f50decc91
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-07 13:18:32 +0300

    getPosts action method completed

---
+++ client/src/actions/post.js
@@ -0,0 +1,23 @@
+import axios from 'axios';
+import { GET_POSTS, POST_ERROR } from './types';
+import { setAlert } from './alert';
+
+//Get Posts :
+export const getPosts = () => async (dispatch) => {
+  try {
+    const res = await axios.get('/api/posts');
+
+    dispatch({
+      type: GET_POSTS,
+      payload: res.data,
+    });
+  } catch (error) {
+    dispatch({
+      type: POST_ERROR,
+      payload: {
+        msg: error.response.statusText,
+        status: error.response.status,
+      },
+    });
+  }
+};
