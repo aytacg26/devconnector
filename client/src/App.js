b8447d867c61f60c64c1ee1ea52da7662703265c
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-07 13:19:16 +0300

    Posts Private Route added to Routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -20,6 +20,7 @@ import Profile from './components/profile/Profile';
 import AddExperience from './components/profile-form/AddExperience';
 import AddEducation from './components/profile-form/AddEducation';
 import Profiles from './components/profiles/Profiles';
+import Posts from './components/posts/Posts';
 
 if (localStorage.getItem('token')) {
   setAuthToken(localStorage.getItem('token'));
@@ -45,6 +46,7 @@ const App = () => {
               <Route path='/profiles' component={Profiles} />
               <Route path='/profile/:id' component={Profile} />
               <PrivateRoute exact path='/dashboard' component={Dashboard} />
+              <PrivateRoute exact path='/posts' component={Posts} />
 
               <PrivateRoute
                 exact
