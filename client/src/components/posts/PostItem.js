5a561763927f76f501994e27d6122376a423194c
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-08 17:47:57 +0300

    Post Item Component Created...

--- client/src/components/posts/PostItem.js
+++ client/src/components/posts/PostItem.js
@@ -1,8 +1,9 @@
-import React, { Fragment } from 'react';
+import React from 'react';
 import PropTypes from 'prop-types';
 import { Link } from 'react-router-dom';
 import Moment from 'react-moment';
 import { useDispatch, useSelector } from 'react-redux';
+import { addLike, deletePost, removeLike } from '../../actions/post';
 
 const PostItem = ({
   post: { _id, text, name, avatar, user, likes, comments, date },
@@ -17,7 +18,7 @@ const PostItem = ({
   return (
     <div className='post bg-white p-1 my-1'>
       <div>
-        <Link to='/profile'>
+        <Link to={`/profile/${user}`}>
           <img className='round-img' src={avatar} alt={name} title={name} />
           <h4>{name}</h4>
         </Link>
@@ -27,11 +28,19 @@ const PostItem = ({
         <p className='post-date'>
           Posted on <Moment format='DD/MM/YYYY'>{date}</Moment>
         </p>
-        <button type='button' className='btn btn-light'>
+        <button
+          type='button'
+          className='btn btn-light'
+          onClick={() => dispatch(addLike(_id))}
+        >
           <i className='fas fa-thumbs-up'></i>{' '}
           {likes.length > 0 && <span>{likes.length}</span>}
         </button>
-        <button type='button' className='btn btn-light'>
+        <button
+          type='button'
+          className='btn btn-light'
+          onClick={() => dispatch(removeLike(_id))}
+        >
           <i className='fas fa-thumbs-down'></i>
         </button>
         <Link to={`/post/${_id}`} className='btn btn-primary'>
@@ -41,7 +50,11 @@ const PostItem = ({
           )}
         </Link>
         {!auth.loading && user === auth.user._id && (
-          <button type='button' className='btn btn-danger'>
+          <button
+            type='button'
+            className='btn btn-danger'
+            onClick={() => dispatch(deletePost(_id))}
+          >
             <i className='fas fa-times'></i>
           </button>
         )}
