338e19c0681483a28b0d7f88b434c23936d1cfc4
Aytac Güley <51089771+aytacg26@users.noreply.github.com>
2021-04-08 17:49:53 +0300

    Comment, Like and Post Cases added to post reducer

--- client/src/reducers/post.js
+++ client/src/reducers/post.js
@@ -1,4 +1,13 @@
-import { GET_POSTS, POST_ERROR } from '../actions/types';
+import {
+  ADD_POST,
+  DELETE_POST,
+  GET_POST,
+  GET_POSTS,
+  POST_ERROR,
+  UPDATE_LIKES,
+  ADD_COMMENT,
+  REMOVE_COMMENT,
+} from '../actions/types';
 
 const initialState = {
   posts: [],
@@ -17,6 +26,56 @@ const postReducer = (state = initialState, action) => {
         loading: false,
         posts: payload,
       };
+
+    case ADD_POST:
+      return {
+        ...state,
+        loading: false,
+        posts: [payload, ...state.posts],
+      };
+
+    case GET_POST:
+      return {
+        ...state,
+        loading: false,
+        post: payload,
+      };
+
+    case DELETE_POST:
+      return {
+        ...state,
+        loading: false,
+        posts: state.posts.filter((post) => post._id !== payload),
+      };
+
+    case ADD_COMMENT:
+      return {
+        ...state,
+        post: { ...state.post, comments: payload },
+        loading: false,
+      };
+
+    case REMOVE_COMMENT:
+      return {
+        ...state,
+        post: {
+          ...state.post,
+          comments: state.post.comments.filter(
+            (comment) => comment._id !== payload
+          ),
+          loading: false,
+        },
+      };
+
+    case UPDATE_LIKES:
+      return {
+        ...state,
+        posts: state.posts.map((post) =>
+          post._id === payload.id ? { ...post, likes: payload.likes } : post
+        ),
+        loading: false,
+      };
+
     case POST_ERROR:
       return {
         ...state,
